333d122e2bd6bbd969b63c8f7a0f8d46f1fa57c4
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2023-02-09 16:14:04 +0100

    work in progress. login might give a console log now if connected properly to api

--- root/client/src/presenter/login.js
+++ root/client/src/presenter/login.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import LoginView from "../view/loginView";
+import { loginRequest } from "../model/person";
 
 /**
  * Login presenter
@@ -10,29 +11,29 @@ export default function Login(props) {
     const navigate = useNavigate();
     const [loginInfo, setLoginInfo] = useState("");
 
-    function loginRequest(formEvent) {
-        fetch("http://localhost:9000/users" , {
-            method: "POST",
-            body: JSON.stringify({
-                userName: formEvent.target.username.value,
-                password: formEvent.target.password.value
-            }),
-            headers: {"Content-type": "application/json; charset=UTF-8"}
-        })
-        .then(res => res.json())
-        .then(res => {
+    // function loginRequest(formEvent) {
+    //     fetch("http://localhost:9000/users" , {
+    //         method: "POST",
+    //         body: JSON.stringify({
+    //             userName: formEvent.target.username.value,
+    //             password: formEvent.target.password.value
+    //         }),
+    //         headers: {"Content-type": "application/json; charset=UTF-8"}
+    //     })
+    //     .then(res => res.json())
+    //     .then(res => {
             
-            console.log(res)
+    //         console.log(res)
 
-            if(res.login === "OK"){
-                props.model.setCurrentUser(res.user);
-                navigate("/");
-            }
-            else {
-                setLoginInfo("Login unsuccessful")
-            }
-        })
-    }
+    //         if(res.login === "OK"){
+    //             props.model.setCurrentUser(res.user);
+    //             navigate("/");
+    //         }
+    //         else {
+    //             setLoginInfo("Login unsuccessful")
+    //         }
+    //     })
+    // }
 
     return (
         <LoginView submitLoginRequest={loginRequest} loginInfo={loginInfo}/>
