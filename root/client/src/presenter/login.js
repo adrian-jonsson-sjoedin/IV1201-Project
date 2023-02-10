44f775691a2a2547e584f4ad25d36da4b918bfd6
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2023-02-10 15:36:37 +0100

    login from client connected to server

--- root/client/src/presenter/login.js
+++ root/client/src/presenter/login.js
@@ -11,31 +11,16 @@ export default function Login(props) {
     const navigate = useNavigate();
     const [loginInfo, setLoginInfo] = useState("");
 
-    // function loginRequest(formEvent) {
-    //     fetch("http://localhost:9000/users" , {
-    //         method: "POST",
-    //         body: JSON.stringify({
-    //             userName: formEvent.target.username.value,
-    //             password: formEvent.target.password.value
-    //         }),
-    //         headers: {"Content-type": "application/json; charset=UTF-8"}
-    //     })
-    //     .then(res => res.json())
-    //     .then(res => {
-            
-    //         console.log(res)
-
-    //         if(res.login === "OK"){
-    //             props.model.setCurrentUser(res.user);
-    //             navigate("/");
-    //         }
-    //         else {
-    //             setLoginInfo("Login unsuccessful")
-    //         }
-    //     })
-    // }
+    async function login(formEvent) {
+        const result = await loginRequest(formEvent, props.model)
+        console.log(result);
+        if (result === "OK") {
+            console.log("test")
+            navigate("/")
+        }
+    }
 
     return (
-        <LoginView submitLoginRequest={loginRequest} loginInfo={loginInfo}/>
+        <LoginView submitLoginRequest={login} loginInfo={loginInfo} />
     );
 }
--- root/client/src/view/homeView.js
+++ root/client/src/view/homeView.js
@@ -4,7 +4,7 @@ export function HomeView(props){
     return (
         <div className="App">
             <header className="App-header">
-                <p>Welcome {!props.user ? "" : props.user.firstName + " " + props.user.lastName}, you are now logged in</p>
+                <p>Welcome {!props.user ? "" : props.user.name + " " + props.user.surname}, you are now logged in</p>
                 <Link to="/login">BACK TO LOGIN PAGE</Link>
             </header>
         </div>
