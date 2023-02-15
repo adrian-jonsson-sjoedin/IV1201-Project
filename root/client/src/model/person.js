7fdda6125d06e575ebe1d4278939fd81399eedb6
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2023-02-15 14:44:00 +0100

    login and update of current user in model implemented

--- root/client/src/model/person.js
+++ root/client/src/model/person.js
@@ -1,5 +1,12 @@
-import Model from "./Model";
-
+/**
+ * Sends a login request to the server and updates the model with the returned 
+ * user if login succeed.
+ * @async 
+ * @param {Event} loginFormEvent - The event object from the login form submission.
+ * @param {Model} model - The model used in the app that will be updated with current user data.
+ * @returns {Promise<string>} A promise that resolves to "OK" if login succeeds, or "Failed" if 
+ * unsuccessful.
+ */
 async function loginRequest(loginFormEvent, model) {
     try {
         const response = await fetch("http://localhost:8080/api/person/login", {
@@ -11,11 +18,11 @@ async function loginRequest(loginFormEvent, model) {
             headers: { "Content-type": "application/json; charset=UTF-8" },
         });
         const responseData = await response.json();
-        console.log('Response data: ', responseData);
+        console.log('Response data: ', responseData); // remove this before publishing app
         if (responseData.status !== "Failed") {
-            console.log('Setting current user: ', responseData);
-            model.setCurrentUser(responseData);
-            console.log('Current user: ', model.currentUser);
+            console.log('Setting current user: ', responseData); // remove this before publishing app
+            model.updateCurrentUser(responseData);
+            console.log('Current user: ', model.currentUser); // remove this before publishing app
             return "OK";
         } else {
             return "Failed";
