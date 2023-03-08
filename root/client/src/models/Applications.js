457031d0f68b722e292a784f14a1d89ff4c154f3
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2023-03-08 14:42:04 +0100

    fetch applications done.

--- root/client/src/models/Applications.js
+++ root/client/src/models/Applications.js
@@ -1,22 +1,22 @@
 import { capitalizeFirstLetter } from "../util/util";
 /**
- * Fetches the list of competences from the API and returns a mapped array of competences.
+ * Fetches the list of applications from the API and returns a mapped array of applications.
  *
  * @async
- * @function fetchCompetenceList
- * @returns {Promise<Array<{competence_id: number, name: string}>>|string} An array of mapped competences or a string "Failed".
+ * @function fetchAllApplications
+ * @returns {Promise<Array<{name: string, surname: string, application_status: string}>>|string} An array of all the application with full name and status, or a string "Failed".
  * @throws {Error} If an error occurs during the network request.
  */
 async function fetchAllApplications() {
     try {
-        const response = await fetch("http://localhost:8080/api/applications");
+        const response = await fetch("http://localhost:8080/api/application");
         const responseData = await response.json();
         console.log("Response data for fetching the all applications: ", responseData);// remove this before publishing app
         if (responseData.status !== 500) {
             const applications = responseData.map(res => ({
-                name: capitalizeFirstLetter(res.name),
-                surname: capitalizeFirstLetter(res.surname),
-                status: capitalizeFirstLetter(res.application_status) 
+                name: capitalizeFirstLetter(res.person.name),
+                surname: capitalizeFirstLetter(res.person.surname),
+                status: capitalizeFirstLetter(res.application_status)
             }));
             console.log("Returned object: ", applications);
             return applications;
@@ -25,8 +25,8 @@ async function fetchAllApplications() {
         }
     } catch (error) {
         console.error(error);
-        throw new Error("An error occurred while trying to retrieve all applications from the server."); 
+        throw new Error("An error occurred while trying to retrieve all applications from the server.");
     }
 }
 
-export {fetchCompetenceList}
+export { fetchAllApplications }
