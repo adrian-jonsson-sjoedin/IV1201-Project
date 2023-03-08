440326d804b8257a0ff65f5acf38b6916618ec1d
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2023-03-08 16:30:18 +0100

    trying to fix cors

--- root/server/server.js
+++ root/server/server.js
@@ -8,7 +8,8 @@ require('dotenv').config();
 
 const cors = require("cors");
 const corsOptions = {
-  origin: ["http://localhost:8081", " https://iv1201.netlify.app"]
+  // origin: ["http://localhost:8081", " https://iv1201.netlify.app"]
+  origin: " https://iv1201.netlify.app"
 };
 
 app.use(cors(corsOptions));
