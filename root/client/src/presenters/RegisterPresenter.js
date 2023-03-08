8fd586ed51e0f30eea0ff6e0cd92ae7280de3282
wilfre-oss <63850794+wilfre-oss@users.noreply.github.com>
2023-03-08 11:37:46 +0100

    login and register server error

--- root/client/src/presenters/RegisterPresenter.js
+++ root/client/src/presenters/RegisterPresenter.js
@@ -12,41 +12,21 @@ export default function Register(props) {
 
     async function register(formEvent) {
 
-        const formData = Object.fromEntries(new FormData(formEvent.taget)) //Full form data as JSON
-        console.log(formData) // remove before publishing
+        const formData = Object.fromEntries(new FormData(formEvent.taget))
 
-        const result = await createApplicantRequest(formEvent, props.model)
-        if (result === "OK") {
-            console.log("Login Successful") // remove this before publishing app
-            setRegisterInfo("OK")
-        }else {
-            setRegisterInfo("Register unsuccessful")
+        try{
+            const result = await createApplicantRequest(formEvent, props.model)
+            if (result === "OK") {
+                console.log("Login Successful") // remove this before publishing app
+                setRegisterInfo("OK")
+            }else {
+                setRegisterInfo("Register unsuccessful")
+            }
+        }
+        catch (err){
+            setRegisterInfo("Server Connection error")
         }
     }
-
-    // function registerRequest(formEvent){
-    //     fetch("http://localhost:9000/register" , {
-    //         method: "POST",
-    //         body: JSON.stringify({
-    //             firstName: formEvent.target.firstName.value,
-    //             lastName: formEvent.target.lastName.value,
-    //             userName: formEvent.target.userName.value,
-    //             email: formEvent.target.email.value,
-    //             password: formEvent.target.password.value
-    //         }),
-    //         headers: {"Content-type": "application/json; charset=UTF-8"}
-    //     })
-    //     .then(res => res.json())
-    //     .then(res => {
-        
-    //         if(res.register === "OK"){
-    //             navigate("/login");
-    //         }
-    //         else {
-    //             setRegisterInfo("Register unsuccessful")
-    //         }
-    //     })
-    // }
 
     if(registerInfo === "OK"){
         return (<RegisterSuccess />);
