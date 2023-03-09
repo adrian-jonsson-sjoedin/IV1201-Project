07406e597fcd31f3326dca82b2449e3b2d14f2ea
wilfre-oss <63850794+wilfre-oss@users.noreply.github.com>
2023-03-09 13:58:32 +0100

    done

--- root/client/src/views/ListApplications.js
+++ root/client/src/views/ListApplications.js
@@ -1,14 +1,21 @@
 
 export function ListApplications({applications}){
-
-    return (
+    
+    return applications ? (
         <div className="container">
         <div className="row justify-content-center my-5">
         <div className="col-lg-6">
-            <h1>Hello!</h1>
+        
+        <table>
+        {applications.map( (appli) => {
+                return(
+                    <tr key={appli.surname}><td>{appli.name}</td><td>{appli.surname}</td><td>{appli.status}</td></tr>
+                )
+            })}
+        </table>
 
         </div> 
         </div>
         </div>
-    );
+    ) : <div></div>
 }
